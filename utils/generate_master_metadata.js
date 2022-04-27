bb19a2141de5c774f9740517fcbbe5e409133be8
Michael Malaspina <michaelmalaspina46@gmail.com>
2022-04-27 22:28:00 +0200

    Added generate_master_metadata.js shuffled path and json extension configs

--- utils/generate_master_metadata.js
+++ utils/generate_master_metadata.js
@@ -1,18 +1,30 @@
 const fs = require('fs');
 const path = require('path');
 const basePath = process.cwd();
-const buildDir = `${basePath}/../build/json`;
 const console = require('console');
 const metadataList = [];
+
+const config = {
+    metadataAreShuffled: true, // true id the metadata files are in the "shuffled_json" path
+    metadataFilesHaveExtension: false, // true if the metadata files have the ".json" extension
+};
+
+const buildDir = config.metadataAreShuffled ? `${basePath}/build/shuffled_json` : `${basePath}/build/json`;
 
 const getJsons = (_dir) => {
     try {
         return fs
             .readdirSync(_dir)
             .filter((item) => {
-                let extension = path.extname(`${_dir}${item}`);
-                if (extension === '.json' && item !== '_metadata.json') {
-                    return item;
+                if (config.metadataFilesHaveExtension) {
+                    let extension = path.extname(`${_dir}${item}`);
+                    if (extension === '.json' && item !== '_metadata.json') {
+                        return item;
+                    }
+                } else {
+                    if (item !== '_metadata.json' && !isNaN(item)) {
+                        return item;
+                    }
                 }
             })
             .sort((a, b) => {
